91666967caebf6eee24279627c2b4e9e2f0e42e8
C-loud-Nine <abdullah.shafi99@gmail.com>
2024-01-20 04:04:32 +0600

    Last Commit D-1

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+} 
+
+$(document).ready(function(){
+
+    var typed = new Typed(".typing", {
+        strings: ["CS Undergrad", "Amateur", "Travel Enthusiast"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+});
